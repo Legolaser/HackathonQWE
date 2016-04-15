5e509e219742cc4d640eb5a1b836c3a0c3eeccea
Legolaser <jirkaseidl@seznam.cz>
2016-04-15 22:04:28 +0200

    funkcni mapa+gps

--- QWEApp/public_html/js/controller.js
+++ QWEApp/public_html/js/controller.js
@@ -1,24 +1,39 @@
 
+var currentLocation;
 $(document).ready(function () {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    
+    function getMap() {
+//        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+        var mymap = L.map('mapid').setView([currentLocation.coords.latitude, currentLocation.coords.longitude], 15);
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-                    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            id: 'mapbox.streets'
-    }).addTo(mymap);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                id: 'mapbox.streets'
+        }).addTo(mymap);
 
-    var greenIcon = L.icon({
-        iconUrl: 'images/Bod.png',
+        var greenIcon = L.icon({
+            iconUrl: 'images/Bod.png',
 
-        iconSize:     [32, 32], // size of the icon
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
+            iconSize:     [32, 32], // size of the icon
+            iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
 
-    L.marker([51.5, -0.09], {icon: greenIcon}).addTo(mymap);
-    //var marker = L.marker([51.5, -0.09]).addTo(mymap);
+        L.marker([currentLocation.coords.latitude, currentLocation.coords.longitude], {icon: greenIcon}).addTo(mymap);
+    }
+    
+    function getLocation() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            currentLocation = position;
+            getMap();
+//        alert("Latitude: " + position.coords.latitude +
+//        "<br>Longitude: " + position.coords.longitude);
+        });
+    }
+    
+    getLocation();
 });
 
